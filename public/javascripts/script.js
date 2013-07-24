95cd02e7aa452d48747a8a1af101c4464ac57dd0
Sohel <sohel@snowflax.com>
2013-07-24 17:24:28 +0530

    Initial commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,168 @@
+
+var socket = io.connect(''); // socket connection
+
+socket.on('connect',function(){
+
+	$.ajax({
+		'url' : '/list',
+		'method' : 'GET',
+		'success' : function(data){
+			if(data){
+				data.forEach(function(d){
+					$('#table').append('<tr id=' + d._id + '>' +
+						'<td><b>' + d.name + '</b></td>' +
+						'<td>' + d.rate + '</td>' +
+						'<td>' +
+						'<img id="vote" src="images/vote.png"/>&nbsp;' +
+						'</td>' +
+						'</tr>');
+				});
+			}
+		}
+	});
+	//socket.emit('new',{data : ''});
+})
+
+
+socket.on('update', function(data) {
+	var td = $('#'+data._id).find('td')[1];
+	$(td).html(data.rate);
+});
+
+socket.on('addnew_ack', function(d) {
+	var td = '';
+
+	$('#table tr td:nth-child(2)').each(function() {
+		if ($(this).text() == parseInt(d.rate)) {
+			td = this;
+		}
+	});
+	if ($("#table tr").length >= 10) {
+		$('#table tr')[$('table tr').length - 1].remove();
+	}
+	$('#table').append('<tr id=' + d._id + '>' +
+			'<td><b>' + d.name + '</b></td>' +
+			'<td>' + d.rate + '</td>' +
+			'<td>' +
+			'<img id="vote" src="images/vote.png"/>&nbsp;' +
+			'</td>' +
+			'</tr>');
+	/*if (d.rate > parseInt($('#table tr td:nth-child(2)')[0].innerHTML)) {
+		$('#table').prepend('<tr id=' + d._id + '>' +
+			'<td><b>' + d.name + '</b></td>' +
+			'<td>' + d.rate + '</td>' +
+			'<td>' +
+			'<img id="vote" src="images/vote.png"/>&nbsp;' +
+			'</td>' +
+			'</tr>');
+	} else if(d.rate < parseInt($('#table tr td:nth-child(2)')[ $('#table tr').length -1].innerHTML)){
+		$('#table').append('<tr id=' + d._id + '>' +
+			'<td><b>' + d.name + '</b></td>' +
+			'<td>' + d.rate + '</td>' +
+			'<td>' +
+			'<img id="vote" src="images/vote.png"/>&nbsp;' +
+			'</td>' +
+			'</tr>');
+	} else{
+		$('<tr id=' + d._id + '>' +
+			'<td><b>' + d.name + '</b></td>' +
+			'<td>' + d.rate + '</td>' +
+			'<td>' +
+			'<img id="vote" src="images/vote.png"/>&nbsp;' +
+			'</td>' +
+			'</tr>').insertAfter($(td).parent());
+	}
+*/
+});
+
+$(document).ready(function() {
+	$(".table").on('click', 'img',function(event){
+		var id = $(event.target).parent().parent().attr('id');
+		socket.emit('update_score', { user_id : id });
+	});
+
+	$("#addnew").on('click', 'img', function(){
+		if(document.getElementById('addnewform').style.display == 'none'){
+			$('#addnewform').css('display', 'block');
+		} else {
+			$('#addnewform').css('display', 'none');
+		}
+	});
+
+	$("#addnewbtn").click(function(){
+		var name = $('#name').val();
+		var score = $('#score').val();
+
+		if(!name.match(/^[a-zA-Z]*$/) ){
+			alert('Enter valid data');
+			console.log(name);
+			return false;
+		} else if(isNaN(parseInt(score))){
+			alert('Enter valid data');
+			console.log(score);
+			return false;
+		} else{
+			socket.emit('addnew', {name: name, rate: parseInt(score)});
+			$("#addnewform").hide();
+		}
+
+	});
+});
+
+socket.on('disconnect', function() {
+	console.log('disconnected..');
+});
+/*
+function LeaderBoardCtrl($scope, $http) {
+
+  $http({method: "GET", url: "/list"}).
+  success(function(data, status, headers, config) {
+	//console.log(status);
+    $scope.users = data;
+	//console.log(data);
+	$.chartBar($scope.users);
+  }).
+  error(function(data, status, headers, config) {
+	  console.log("err : "+status);
+  });
+
+  $scope.rating = function(id){
+
+	  socket.emit('id', { user_id : id });
+
+    }
+
+
+  socket.on('update', function (data) {
+	 	 $http({method: 'GET', url: '/list'}).
+		  success(function(data2, status, headers, config) {
+			  //console.log("data");
+			$scope.users = data2;
+			$.chartBar($scope.users);
+			//console.log("ok server send data to angular scope");
+		  }).
+		  error(function(data2, status, headers, config) {
+			  console.log("err : "+data2);
+		  });
+	});
+
+	$scope.total = function(){
+		var total=0;
+		angular.forEach($scope.users, function(item){
+			total+=item.rate;
+        });
+        return total;
+	};
+
+	$scope.highest = function(){
+		var max=0;
+		var high = [];
+		angular.forEach($scope.users, function(item){
+			high.push(item.rate);
+        });
+		max = Math.max.apply(Math,high);
+        return max;
+	};
+
+}
+*/
---
+++ routes/index.js
@@ -0,0 +1,101 @@
+
+/*
+ * GET home page.
+ */
+// import database
+var mongo = require('mongodb');
+
+//create database server
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+//var io = require('socket.io').listen(server, { log: false });
+
+// create cartdb Database
+db = new Db('players_db', server);
+
+// open database
+db.open(function(err, db) {
+    if(!err) {
+        db.collection('users', {safe:true}, function(err, collection) {
+
+           if (err) {
+                console.log("The 'users' collection doesn't exist. Creating it with sample data..."+err);
+                populateDB();
+            }
+			else{
+				//populateDB();
+				console.log("The 'user' collection exists.. "+collection);
+
+				collection.find().toArray(function(err, items) {
+					 if(items.length == 0){
+					  	populateDB();
+					 	//console.log("Length-1:::"+items.length);
+					 	//console.log(items);
+					 }else{
+					 	//console.log("Length-2:::"+items.length);
+					 }
+				});
+			}
+        });
+    }
+});
+
+exports.list = function(req, res){
+	console.log("call List....");
+	db.collection('users', function(err, collection) {
+		collection.find({}).sort({rate: -1}).limit(10).toArray(function(err, items) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		res.json(items);
+		});
+	});
+};
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+
+    var user = [
+    {
+		name: "Ada Lovelace",
+		rate: 0
+	},
+    {
+		name: "Grace Hopper",
+		rate: 0
+	},
+    {
+		name: "Marie Curie",
+		rate: 0
+	},
+    {
+		name: "Claude Shannon",
+		rate: 0
+	},
+    {
+		name: "Nikola Tesla",
+		rate: 0
+	}
+	];
+
+    db.collection('users', function(err, collection) {
+
+		collection.remove(); //remove database
+
+		collection.insert(user, {safe:true}, function(err, result) {
+			if(err){
+				console.log(err);
+			}
+			else{
+				//console.log(result);
+			}
+		});
+    });
+
+};
