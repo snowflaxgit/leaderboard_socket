7dca59a45875b69293057dfd14094e952cf2d6dd
Sohel <sohel@snowflax.com>
2013-08-01 15:36:41 +0530

    Some bug fixes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,38 +1,32 @@
 
 var socket = io.connect(''); // socket connection
 
-socket.on('connect',function(){
-
-	$.ajax({
-		'url' : '/list',
-		'method' : 'GET',
-		'success' : function(data){
-			if(data){
-				data.forEach(function(d){
-					$('#table').append('<tr id=' + d._id + '>' +
-						'<td><b>' + d.name + '</b></td>' +
-						'<td>' + d.rate + '</td>' +
-						'<td>' +
-						'<img id="vote" src="images/vote.png"/>&nbsp;' +
-						'</td>' +
-						'</tr>');
-				});
-			}
-		}
-	});
-	//socket.emit('new',{data : ''});
+socket.on('connect',function(data){
+	console.log(data);
+	if (data) {
+		data.forEach(function(d) {
+			$('#table').append('<tr id=' + d._id + '>' +
+				'<td><b>' + d.name + '</b></td>' +
+				'<td>' + d.rate + '</td>' +
+				'<td>' +
+				'<img id="vote" src="images/vote.png"/>&nbsp;' +
+				'</td>' +
+				'</tr>');
+		});
+	}
 })
 
 function deleteSingle(id){
 	socket.emit('delete_single', id);
 }
 
-
 socket.on('update_score_ack', function(data) {
 	console.log(data);
 	var td = $('#'+data._id).find('td')[1];
 	$(td).html(data.rate);
 });
+
+
 
 socket.on('addnew_ack', function(d) {
 	var td = '';
@@ -52,32 +46,6 @@ socket.on('addnew_ack', function(d) {
 			'<img id="vote" src="images/vote.png"/>&nbsp;' +
 			'</td>' +
 			'</tr>');
-	/*if (d.rate > parseInt($('#table tr td:nth-child(2)')[0].innerHTML)) {
-		$('#table').prepend('<tr id=' + d._id + '>' +
-			'<td><b>' + d.name + '</b></td>' +
-			'<td>' + d.rate + '</td>' +
-			'<td>' +
-			'<img id="vote" src="images/vote.png"/>&nbsp;' +
-			'</td>' +
-			'</tr>');
-	} else if(d.rate < parseInt($('#table tr td:nth-child(2)')[ $('#table tr').length -1].innerHTML)){
-		$('#table').append('<tr id=' + d._id + '>' +
-			'<td><b>' + d.name + '</b></td>' +
-			'<td>' + d.rate + '</td>' +
-			'<td>' +
-			'<img id="vote" src="images/vote.png"/>&nbsp;' +
-			'</td>' +
-			'</tr>');
-	} else{
-		$('<tr id=' + d._id + '>' +
-			'<td><b>' + d.name + '</b></td>' +
-			'<td>' + d.rate + '</td>' +
-			'<td>' +
-			'<img id="vote" src="images/vote.png"/>&nbsp;' +
-			'</td>' +
-			'</tr>').insertAfter($(td).parent());
-	}
-*/
 });
 
 socket.on('delete_single_ack', function(data){
@@ -124,57 +92,3 @@ $(document).ready(function() {
 socket.on('disconnect', function() {
 	console.log('disconnected..');
 });
-/*
-function LeaderBoardCtrl($scope, $http) {
-
-  $http({method: "GET", url: "/list"}).
-  success(function(data, status, headers, config) {
-	//console.log(status);
-    $scope.users = data;
-	//console.log(data);
-	$.chartBar($scope.users);
-  }).
-  error(function(data, status, headers, config) {
-	  console.log("err : "+status);
-  });
-
-  $scope.rating = function(id){
-
-	  socket.emit('id', { user_id : id });
-
-    }
-
-
-  socket.on('update', function (data) {
-	 	 $http({method: 'GET', url: '/list'}).
-		  success(function(data2, status, headers, config) {
-			  //console.log("data");
-			$scope.users = data2;
-			$.chartBar($scope.users);
-			//console.log("ok server send data to angular scope");
-		  }).
-		  error(function(data2, status, headers, config) {
-			  console.log("err : "+data2);
-		  });
-	});
-
-	$scope.total = function(){
-		var total=0;
-		angular.forEach($scope.users, function(item){
-			total+=item.rate;
-        });
-        return total;
-	};
-
-	$scope.highest = function(){
-		var max=0;
-		var high = [];
-		angular.forEach($scope.users, function(item){
-			high.push(item.rate);
-        });
-		max = Math.max.apply(Math,high);
-        return max;
-	};
-
-}
-*/
